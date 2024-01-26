552b65fa78c2ddb3a2d925939453d22846ab720a
pc <betul.bicer@etu.edu.tr>
2024-01-26 16:55:44 +0300

    ekle sil

--- src/main/java/com/egitim/ders/controller/KullaniciController.java
+++ src/main/java/com/egitim/ders/controller/KullaniciController.java
@@ -2,17 +2,13 @@ package com.egitim.ders.controller;
 
 import com.egitim.ders.model.Kullanici;
 import com.egitim.ders.service.KullaniciService;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/kullanici")
 public class KullaniciController {
-
     final KullaniciService kullaniciService;
 
     public KullaniciController(KullaniciService kullaniciService) {
@@ -24,5 +20,18 @@ public class KullaniciController {
         return kullaniciService.getAllKullanici();
     }
 
+    @GetMapping("/id")
+    public Kullanici getKullaniciById(@RequestParam Long id) {
+        return kullaniciService.getKullaniciById(id);
+    }
 
+    @PostMapping("/add")
+    public Kullanici addKullanici(@RequestBody Kullanici kullanici){
+        return kullaniciService.addKullanici(kullanici);
+    }
+    @PutMapping("/edit")
+    public Kullanici editKullanici(@RequestParam Long id,@RequestBody Kullanici kullanici){
+        return kullaniciService.editKullanici(id,kullanici);
+    }
 }
+
--- src/main/java/com/egitim/ders/service/KullaniciService.java
+++ src/main/java/com/egitim/ders/service/KullaniciService.java
@@ -3,6 +3,8 @@ package com.egitim.ders.service;
 import com.egitim.ders.model.Kullanici;
 import com.egitim.ders.repository.KullaniciRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -14,9 +16,25 @@ public class KullaniciService {
         this.kullaniciRepository = kullaniciRepository;
     }
 
-
-
     public List<Kullanici> getAllKullanici() {
         return kullaniciRepository.findAll();
+    }
+
+    public Kullanici getKullaniciById(Long id) {
+        return kullaniciRepository.findById(id).orElse(null);
+    }
+
+
+    public Kullanici addKullanici(Kullanici kullanici){
+        return kullaniciRepository.save(kullanici);
+    }
+
+
+    public Kullanici editKullanici(Long id, Kullanici kullanici) {
+        Kullanici kullanicidb = kullaniciRepository.findById(id).orElseThrow(() -> new IllegalStateException("Böyle bir kullanıcı yok!!"));
+        kullanicidb.setUsername(kullanici.getUsername());
+        kullanicidb.setFirstname(kullanici.getFirstname());
+        kullanicidb.setSurname(kullanici.getSurname());
+        return kullaniciRepository.save(kullanicidb);
     }
 }
